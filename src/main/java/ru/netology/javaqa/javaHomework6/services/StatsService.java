7d872687c553d81a0d46ec3d3373c25f75634c07
Ekaterina Pedan <kat.pedan@yandex.ru>
2024-06-07 23:23:48 +0500

    Mistakes fixed

--- src/main/java/ru/netology/javaqa/javaHomework6/services/StatsService.java
+++ src/main/java/ru/netology/javaqa/javaHomework6/services/StatsService.java
@@ -10,38 +10,38 @@ public class StatsService {
         return amount;
     }
 
-    public int averageSales(long[] sales) {
+    public long averageSales(long[] sales) {
         long amount = amountSales(sales);
-        return (int) amount / sales.length;
+        return amount / sales.length;
     }
 
     public int maxSales(long[] sales) {
-        int maxMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+        int maxMonth = 0;
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] >= sales[maxMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
-                maxMonth = i; // запомним его как минимальный
+            if (sales[i] >= sales[maxMonth]) {
+                maxMonth = i;
             }
         }
 
-        return maxMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+        return maxMonth + 1;
     }
 
     public int minSales(long[] sales) {
-        int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+        int minMonth = 0;
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] <= sales[minMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
-                minMonth = i; // запомним его как минимальный
+            if (sales[i] <= sales[minMonth]) {
+                minMonth = i;
             }
         }
 
-        return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+        return minMonth + 1;
     }
 
     public int belowAverage(long[] sales) {
-        double average = averageSales(sales);
+        long average = averageSales(sales);
         int month = 0;
-        for (long i : sales) {
-            if (i < average) {
+        for (long sale : sales) {
+            if (sale < average) {
                 month++;
             }
         }
@@ -49,10 +49,10 @@ public class StatsService {
     }
 
     public int aboveAverage(long[] sales) {
-        double average = averageSales(sales);
+        long average = averageSales(sales);
         int month = 0;
-        for (long i : sales) {
-            if (i > average) {
+        for (long sale : sales) {
+            if (sale > average) {
                 month++;
             }
         }
